8c6042331181463b3ce23c27f7e0c94dae37672a
ali <ali.s.tag2000@gmail.com>
2023-08-20 09:16:28 +0430

    Edit midline based on shear wall exist. Cut post , beam, shearWall sync part and paste them in separate files.

--- stableVersion2/Sync/mainSync.py
+++ stableVersion2/Sync/mainSync.py
@@ -1,22 +1,19 @@
 import sys
 
-sys.path.append(r"D:\git\Wood\UI_Wood\stableVersion1")
+sys.path.append(r"D:\git\Wood\UI_Wood\stableVersion2")
 sys.path.append(r"D:\git\Wood")
-from output.beam_output import beam_output
-from output.post_output import post_output
-from output.joist_output import Joist_output
-from output.shearWall_output import ShearWall_output, EditLabel
-from WOOD_DESIGN.mainpost import MainPost
+from UI_Wood.stableVersion2.output.joist_output import Joist_output
+from UI_Wood.stableVersion2.output.shearWallSql import MidlineSQL, SeismicParamsSQL
 from WOOD_DESIGN.mainbeamnew import MainBeam
 from WOOD_DESIGN.mainshearwall import MainShearwall
 from WOOD_DESIGN.reports import Sqlreports
-from Sync.data import Data
-from Sync.Image import saveImage
-from Sync.reaction import Control_reaction, Reaction_On
-from post_new import magnification_factor
-from Report_Lab.version1.main import Main
-from Report_Lab.version1.beam.input import BeamInput
-from Report_Lab.version1.post.input import PostInput
+from UI_Wood.stableVersion2.Sync.data import Data
+from UI_Wood.stableVersion2.Sync.Image import saveImage
+from UI_Wood.stableVersion2.Sync.postSync import PostSync
+from UI_Wood.stableVersion2.Sync.beamSync import beamAnalysisSync
+from UI_Wood.stableVersion2.Sync.shearWallSync import ShearWallSync, ControlSeismicParameter, ControlMidLine, \
+    NoShearWallLines, MidlineEdit
+from UI_Wood.stableVersion2.post_new import magnification_factor
 
 
 class mainSync(Data):
@@ -28,9 +25,9 @@ class mainSync(Data):
 
     def Run_and_Analysis(self):
         midLineDict = {}
-
+        lineLabels = None
         for currentTab in range(self.tabWidgetCount - 1, -1, -1):
-            midLineData = self.grid[currentTab].run_control()
+            midLineData, lineLabels = self.grid[currentTab].run_control()
             if currentTab == self.tabWidgetCount - 1:
                 storyName = "Roof"
             else:
@@ -46,13 +43,18 @@ class mainSync(Data):
         # LoadMapMagBefore = TabData.loadMapMag
         JoistArea = TabData.joistArea
         storyName = TabData.storyName
-        LoadMapaArea, LoadMapMag = LoadMapAreaNew(midLineDict)
+        noShearWallLines = NoShearWallLines(set(lineLabels))
+        midLineInstance = MidlineEdit(lineLabels, midLineDict, noShearWallLines)
+        midLineDictEdited = midLineInstance.newMidline
+        boundaryLineNoShearWall = midLineInstance.boundaryLineNoShearWall
+        LoadMapaArea, LoadMapMag = LoadMapAreaNew(midLineDictEdited)
         seismicInstance = ControlSeismicParameter(self.seismic_parameters, storyName, LoadMapaArea, LoadMapMag,
                                                   JoistArea)
+        ControlMidLine(midLineDictEdited)
 
         print(seismicInstance.seismicPara)
-        print(midLineDict)
-        MainShearwall(seismicInstance.seismicPara, midLineDict)
+        print(midLineDictEdited)
+        MainShearwall(seismicInstance.seismicPara, midLineDictEdited)
 
         print("FINAL")
 
@@ -74,15 +76,6 @@ class ControlGeneralProp:
             item = [10 * magnification_factor]  # 10 ft or 10 m
 
         return item
-
-
-class ControlSeismicParameter:
-    def __init__(self, seismicPara, storyName, areaLoad, magLoad, areaJoist):
-        self.seismicPara = seismicPara
-        self.seismicPara["story_name"] = storyName
-        self.seismicPara["load_area"] = areaLoad
-        self.seismicPara["load_magnitude"] = magLoad
-        self.seismicPara["joist_area"] = areaJoist
 
 
 class ControlTab:
@@ -130,113 +123,6 @@ class ControlTab:
         # print("ALL POSTS : ", self.posts)
         # print("ALL BEAMS : ", self.beams)
         print("ALL SHEAR WALLS : ", self.shearWalls)
-
-
-class beamAnalysisSync:
-    def __init__(self, beam, Posts, ShearWalls, generalInfo, db):
-        self.beam = beam
-        self.reactionTab = []
-        self.reaction_list = []
-        beamId = 1
-
-        for i, beamTab in enumerate(beam):
-            self.reaction_list.clear()
-            beamOutput = beam_output(beamTab)
-
-            tabNumber = i
-            beam_support = []
-
-            for Beam in beamTab:
-                beamSupport = Beam["support"]
-                for support in beamSupport:
-                    supportLabel = support["label"]
-                    if "B" in supportLabel:
-                        beam_support.append(supportLabel)
-
-            for beamNum, beam_ in enumerate(beamOutput.beamProperties):
-                # beam with no support are empty(False)
-                if beam_:
-                    label = beam_["label"]
-                    if label not in beam_support:
-                        # BEAM SHOULD ANALYZE FIRST
-                        beam_analysis = MainBeam(beam_)
-                        if beam_analysis.query[0] != "No Section Was Adequate":
-                            beam_analysis.query.insert(0, str(beamId))
-                            beam_analysis.query.insert(1, str(tabNumber + 1))
-                            beam_analysis.query.insert(2, beam_["label"])
-
-                            db.cursor1.execute(
-                                'INSERT INTO BEAM (ID, STORY, LABEL, LENGTH, SIZE,Vmax, Mmax, Fb_actual, Fb_allow, Fv_actual, Fv_allow, Deflection_actual, Deflection_allow, Bending_dcr, Shear_dcr,DIST_D, DIST_D_range, DIST_L, DIST_L_range, DIST_LR, DIST_LR_range, DIST_E, DIST_E_range, P_D, P_D_range, P_L, P_L_range, P_LR, P_LR_range, P_E, P_E_range,RD, RL, RLr, RE) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?,  ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                                beam_analysis.query)
-                            db.conn1.commit()
-
-                            Control_reaction(beam_analysis.output.post_output, beamTab[beamNum], self.reaction_list)
-                            beamId += 1
-                            pass
-
-            # # assign beam reactions
-            # for reaction in self.reaction_list:
-            #     assign_beam_reaction(reaction, self.beam)
-
-            reactionInstance = Reaction_On(beamTab, Posts[tabNumber], ShearWalls[tabNumber], self.reaction_list)
-            reactionInstance.do_beam()
-
-            beamOutput = beam_output(beamTab)
-            for beamNum, beam_ in enumerate(beamOutput.beamProperties):
-                InputReport = BeamInput(beam_, generalInfo)
-                value = InputReport.input
-                report = Main(value, value, value,
-                              f"D:/git/Wood/UI_Wood/stableVersion1/report/Beam_output_{beam_['label']}_story_{str(i)}.pdf")
-                # beam with no support are empty(False)
-                if beam_:
-                    if beam_["label"] in beam_support:
-                        beam_analysis = MainBeam(beam_)
-                        if beam_analysis.query[0] != "No Section Was Adequate":
-                            beam_analysis.query.insert(0, str(beamId))
-                            beam_analysis.query.insert(1, str(tabNumber + 1))
-                            beam_analysis.query.insert(2, beam_["label"])
-
-                            db.cursor1.execute(
-                                'INSERT INTO BEAM (ID, STORY, LABEL, LENGTH, SIZE,Vmax, Mmax, Fb_actual, Fb_allow, Fv_actual, Fv_allow, Deflection_actual, Deflection_allow, Bending_dcr, Shear_dcr,DIST_D, DIST_D_range, DIST_L, DIST_L_range, DIST_LR, DIST_LR_range, DIST_E, DIST_E_range, P_D, P_D_range, P_L, P_L_range, P_LR, P_LR_range, P_E, P_E_range,RD, RL, RLr, RE) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?,  ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                                beam_analysis.query)
-                            db.conn1.commit()
-                            Control_reaction(beam_analysis.output.post_output, beamTab[beamNum], self.reaction_list)
-                            beamId += 1
-
-                            pass
-
-            reactionInstance.do_post()
-            self.reactionTab.append(self.reaction_list)
-
-
-class PostSync:
-    def __init__(self, posts, height, generalInfo, db):
-        self.postOutPut = post_output(posts, height)
-        print(self.postOutPut.postProperties)
-        postId = 1
-        for post in self.postOutPut.postProperties:
-            InputReport = PostInput(post, generalInfo)
-            value = InputReport.input
-            report = Main(value, value, value,
-                          f"D:/git/Wood/UI_Wood/stableVersion1/report/Post_output_{post['label']}_ID_{str(postId)}.pdf")
-            postAnalysis = MainPost(post)
-            postAnalysis.query.insert(0, postId)
-            postAnalysis.query.insert(1, str(post["story"]))
-            postAnalysis.query.insert(2, post["label"])
-            db.cursor1.execute(
-                'INSERT INTO POST (ID, STORY,LABEL, SIZE, GRADE, Pa_k, fc_psi, Cp, P_allow, Fcc_psi, DCR, Load_comb, Fc_perp, Load_comb_sill, DCR_sill) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
-                postAnalysis.query)
-            db.conn1.commit()
-            postId += 1
-
-
-class ShearWallSync:
-    def __init__(self, shearWall, height, db):
-        shearWallEditedInstance = EditLabel(shearWall)
-        shearWallEdited = list(reversed(shearWallEditedInstance.shearWalls_rev))
-        self.shearWallOutPut = ShearWall_output(shearWallEdited, height)
-        print("*** SHEAR PROP IS HERE", self.shearWallOutPut.shearWallProperties)
-        shearWallId = 1
 
 
 def LoadMapArea(loadMaps):
