5d2bd788c16760685cb889d9c49ede7facf924a3
ali <ali.s.tag2000@gmail.com>
2023-08-21 13:22:18 +0430

    Define InformationSaver class.

---
+++ stableVersion2/InformationSaver.py
@@ -0,0 +1,7 @@
+class InformationSaver:
+    line_edit_projectTitle = None
+    line_edit_company = None
+    line_edit_designer = None
+    line_edit_client = None
+    line_edit_comment = None
+    unit_combo = None
