b09207ecd436d9e57da0b194892b9e791c6e8161
ali <ali.s.tag2000@gmail.com>
2023-08-21 13:22:46 +0430

    save data in InformationSaver class.

--- stableVersion2/Information.py
+++ stableVersion2/Information.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QLineEdit, QComboBox
 from general_properties import h_layout_control
+from InformationSaver import InformationSaver
 
 
 class Widget_form(QWidget):
@@ -21,36 +22,42 @@ class Widget_form(QWidget):
         projectTitle = QLabel("Project Title :")
         projectTitle.setFixedWidth(100)
         self.line_edit_projectTitle = QLineEdit()
+        InformationSaver.line_edit_projectTitle = self.line_edit_projectTitle
         h_l1 = h_layout_control(projectTitle, self.line_edit_projectTitle)
         self.line_edit_projectTitle.editingFinished.connect(self.project_title_control)
 
         company = QLabel("Company :")
         company.setFixedWidth(100)
         self.line_edit_company = QLineEdit()
+        InformationSaver.line_edit_company = self.line_edit_company
         h_l2 = h_layout_control(company, self.line_edit_company)
         self.line_edit_company.editingFinished.connect(self.company_control)
 
         designer = QLabel("Designer :")
         designer.setFixedWidth(100)
         self.line_edit_designer = QLineEdit()
+        InformationSaver.line_edit_designer = self.line_edit_designer
         h_l3 = h_layout_control(designer, self.line_edit_designer)
         self.line_edit_designer.editingFinished.connect(self.designer_control)
 
         client = QLabel("Client :")
         client.setFixedWidth(100)
         self.line_edit_client = QLineEdit()
+        InformationSaver.line_edit_client = self.line_edit_client
         h_l4 = h_layout_control(client, self.line_edit_client)
         self.line_edit_client.editingFinished.connect(self.client_control)
 
         comment = QLabel("Comment :")
         comment.setFixedWidth(100)
         self.line_edit_comment = QLineEdit()
+        InformationSaver.line_edit_comment = self.line_edit_comment
         h_l5 = h_layout_control(comment, self.line_edit_comment)
         self.line_edit_comment.editingFinished.connect(self.comment_control)
 
         unit = QLabel("Unit System :")
         unit.setFixedWidth(100)
         self.unit_combo = QComboBox()
+        InformationSaver.unit_combo = self.unit_combo
         self.unit_combo.addItems(["US", "Metric"])
         h_l6 = h_layout_control(unit, self.unit_combo)
         self.unit_combo.activated.connect(self.unit_control)
