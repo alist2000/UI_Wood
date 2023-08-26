df905ed6677bd5c317cae28758ea5f79c93262e4
ali <ali.s.tag2000@gmail.com>
2023-08-26 09:43:01 +0430

    1) Create SQL for beam inputs. 2) Debug. Beam design order edited. (Important edit)

--- stableVersion2/Sync/beamSync.py
+++ stableVersion2/Sync/beamSync.py
@@ -6,6 +6,7 @@ from Report_Lab.version1.main import Main
 from UI_Wood.stableVersion2.output.beam_output import beam_output
 from WOOD_DESIGN.mainbeamnew import MainBeam
 from UI_Wood.stableVersion2.Sync.reaction import Control_reaction, Reaction_On
+from UI_Wood.stableVersion2.output.beamSql import beamSQL, WriteBeamInputSQL
 from Report_Lab.version1.beam.input import BeamInput
 
 
@@ -15,7 +16,8 @@ class beamAnalysisSync:
         self.reactionTab = []
         self.reaction_list = []
         beamId = 1
-
+        beamIdInput = 1
+        inputDB = beamSQL()
         for i, beamTab in enumerate(beam):
             self.reaction_list.clear()
             beamOutput = beam_output(beamTab)
@@ -30,33 +32,44 @@ class beamAnalysisSync:
                     if "B" in supportLabel:
                         beam_support.append(supportLabel)
 
-            for beamNum, beam_ in enumerate(beamOutput.beamProperties):
-                # beam with no support are empty(False)
-                if beam_:
-                    label = beam_["label"]
-                    if label not in beam_support:
-                        # BEAM SHOULD ANALYZE FIRST
-                        beam_analysis = MainBeam(beam_)
-                        if beam_analysis.query[0] != "No Section Was Adequate":
-                            beam_analysis.query.insert(0, str(beamId))
-                            beam_analysis.query.insert(1, str(tabNumber + 1))
-                            beam_analysis.query.insert(2, beam_["label"])
+            beamDesignedList = []
+            while beam_support:
+                beam_support, self.reaction_list, beamDesignedList, reactionInstance, beamId = self.DesignPrimaryBeam(
+                    beam_support, beamId,
+                    self.reaction_list,
+                    tabNumber, db, beamTab,
+                    Posts, ShearWalls,
+                    beamDesignedList)
 
-                            db.cursor1.execute(
-                                'INSERT INTO BEAM (ID, STORY, LABEL, LENGTH, SIZE,Vmax, Mmax, Fb_actual, Fb_allow, Fv_actual, Fv_allow, Deflection_actual, Deflection_allow, Bending_dcr, Shear_dcr,DIST_D, DIST_D_range, DIST_L, DIST_L_range, DIST_LR, DIST_LR_range, DIST_E, DIST_E_range, P_D, P_D_range, P_L, P_L_range, P_LR, P_LR_range, P_E, P_E_range,RD, RL, RLr, RE) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?,  ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-                                beam_analysis.query)
-                            db.conn1.commit()
-
-                            Control_reaction(beam_analysis.output.post_output, beamTab[beamNum], self.reaction_list)
-                            beamId += 1
-                            pass
+            # for beamNum, beam_ in enumerate(beamOutput.beamProperties):
+            #     # beam with no support are empty(False)
+            #     if beam_:
+            #         label = beam_["label"]
+            #
+            #         if label not in beam_support:
+            #             # BEAM SHOULD ANALYZE FIRST
+            #             beam_analysis = MainBeam(beam_)
+            #             if beam_analysis.query[0] != "No Section Was Adequate":
+            #                 # WriteBeamInputSQL(beam_, str(tabNumber + 1), beamId, inputDB)
+            #                 beam_analysis.query.insert(0, str(beamId))
+            #                 beam_analysis.query.insert(1, str(tabNumber + 1))
+            #                 beam_analysis.query.insert(2, beam_["label"])
+            #
+            #                 db.cursor1.execute(
+            #                     'INSERT INTO BEAM (ID, STORY, LABEL, LENGTH, SIZE,Vmax, Mmax, Fb_actual, Fb_allow, Fv_actual, Fv_allow, Deflection_actual, Deflection_allow, Bending_dcr, Shear_dcr,DIST_D, DIST_D_range, DIST_L, DIST_L_range, DIST_LR, DIST_LR_range, DIST_E, DIST_E_range, P_D, P_D_range, P_L, P_L_range, P_LR, P_LR_range, P_E, P_E_range,RD, RL, RLr, RE) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?,  ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            #                     beam_analysis.query)
+            #                 db.conn1.commit()
+            #
+            #                 Control_reaction(beam_analysis.output.post_output, beamTab[beamNum], self.reaction_list)
+            #                 beamId += 1
+            #                 pass
 
             # # assign beam reactions
             # for reaction in self.reaction_list:
             #     assign_beam_reaction(reaction, self.beam)
 
-            reactionInstance = Reaction_On(beamTab, Posts[tabNumber], ShearWalls[tabNumber], self.reaction_list)
-            reactionInstance.do_beam()
+            # reactionInstance = Reaction_On(beamTab, Posts[tabNumber], ShearWalls[tabNumber], self.reaction_list)
+            # reactionInstance.do_beam()
 
             beamOutput = beam_output(beamTab)
             for beamNum, beam_ in enumerate(beamOutput.beamProperties):
@@ -66,9 +79,13 @@ class beamAnalysisSync:
                               f"D:/git/Wood/UI_Wood/stableVersion1/report/Beam_output_{beam_['label']}_story_{str(i)}.pdf")
                 # beam with no support are empty(False)
                 if beam_:
-                    if beam_["label"] in beam_support:
+                    WriteBeamInputSQL(beam_, str(tabNumber + 1), beamIdInput, inputDB)
+                    beamIdInput += 1
+                    if beam_["label"] not in beamDesignedList:
                         beam_analysis = MainBeam(beam_)
                         if beam_analysis.query[0] != "No Section Was Adequate":
+                            # WriteBeamInputSQL(beam_, str(tabNumber + 1), beamId, inputDB)
+
                             beam_analysis.query.insert(0, str(beamId))
                             beam_analysis.query.insert(1, str(tabNumber + 1))
                             beam_analysis.query.insert(2, beam_["label"])
@@ -84,3 +101,48 @@ class beamAnalysisSync:
 
             reactionInstance.do_post()
             self.reactionTab.append(self.reaction_list)
+
+    @staticmethod
+    def DesignPrimaryBeam(beam_support, beamId, reaction_list, tabNumber, db, beamTab, Posts, ShearWalls, beamDesigned):
+        beamOutput = beam_output(beamTab)
+        for beamNum, beam_ in enumerate(beamOutput.beamProperties):
+            # beam with no support are empty(False)
+            if beam_:
+                label = beam_["label"]
+
+                if label not in beam_support and label not in beamDesigned:
+                    # BEAM SHOULD ANALYZE FIRST
+                    beam_analysis = MainBeam(beam_)
+                    beamDesigned.append(label)
+                    if beam_analysis.query[0] != "No Section Was Adequate":
+                        # WriteBeamInputSQL(beam_, str(tabNumber + 1), beamId, inputDB)
+                        beam_analysis.query.insert(0, str(beamId))
+                        beam_analysis.query.insert(1, str(tabNumber + 1))
+                        beam_analysis.query.insert(2, beam_["label"])
+
+                        db.cursor1.execute(
+                            'INSERT INTO BEAM (ID, STORY, LABEL, LENGTH, SIZE,Vmax, Mmax, Fb_actual, Fb_allow, Fv_actual, Fv_allow, Deflection_actual, Deflection_allow, Bending_dcr, Shear_dcr,DIST_D, DIST_D_range, DIST_L, DIST_L_range, DIST_LR, DIST_LR_range, DIST_E, DIST_E_range, P_D, P_D_range, P_L, P_L_range, P_LR, P_LR_range, P_E, P_E_range,RD, RL, RLr, RE) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?,  ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                            beam_analysis.query)
+                        db.conn1.commit()
+
+                        Control_reaction(beam_analysis.output.post_output, beamTab[beamNum], reaction_list)
+                        beamId += 1
+                        pass
+
+        # # assign beam reactions
+        # for reaction in self.reaction_list:
+        #     assign_beam_reaction(reaction, self.beam)
+
+        reactionInstance = Reaction_On(beamTab, Posts[tabNumber], ShearWalls[tabNumber], reaction_list)
+        reactionInstance.do_beam()
+
+        reactionPass = []
+        for reaction in reaction_list:
+            reactionLabel = reaction["label"]
+            reactionPass.append(reactionLabel)
+        new_beam_support = []
+        for beamLabel in beam_support:
+            if beamLabel not in reactionPass:
+                new_beam_support.append(beamLabel)
+
+        return new_beam_support, reaction_list, beamDesigned, reactionInstance, beamId
