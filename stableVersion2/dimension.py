fed81f236840cf8ea8e7dd471fee0f8330911406
ali <ali.s.tag2000@gmail.com>
2023-09-02 10:39:23 +0430

    Define CoordinateLabel class for creating label for post. add transparency for labels.

--- stableVersion2/dimension.py
+++ stableVersion2/dimension.py
@@ -3,5 +3,11 @@ from PySide6.QtWidgets import QLabel
 
 def DimensionLabel(length, magnification_factor):
     dimension = QLabel(str(round(abs(length / magnification_factor), 2)) + " ft(m)")
-    dimension.setStyleSheet("QLabel { background-color : white; color : black; }")
+    dimension.setStyleSheet("QLabel { background-color :rgba(255, 255, 255, 0); color : black; }")
+    return dimension
+
+
+def CoordinateLabel(x, y, magnification_factor):
+    dimension = QLabel(f"({str(round(x / magnification_factor, 2))}, {str(round(y / magnification_factor, 2))})")
+    dimension.setStyleSheet("QLabel { background-color : rgba(255, 255, 255, 0); color : black; }")
     return dimension
