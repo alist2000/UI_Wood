af1d40ab38cf7f0549b7c392d84c740e4e81494d
ali <ali.s.tag2000@gmail.com>
2023-09-11 11:44:55 +0430

    Design stud wall. Use MainStud class.

--- stableVersion2/Sync/studWallSync.py
+++ stableVersion2/Sync/studWallSync.py
@@ -3,6 +3,7 @@ import string
 
 from UI_Wood.stableVersion2.output.studWall_output import StudWall_output
 from UI_Wood.stableVersion2.output.shearWall_output import EditLabel
+from WOOD_DESIGN.mainstud import MainStud
 
 
 class StudWallSync:
@@ -11,4 +12,6 @@ class StudWallSync:
         studWallEdited = list(reversed(studWallEditedInstance.shearWalls_rev))
         self.studWallOutPut = StudWall_output(studWallEdited, height)
         print("*** STUD PROP IS HERE", self.studWallOutPut.studWallProperties)
+        studInstance = MainStud()
+        print(studInstance.query)
         studWallId = 1
