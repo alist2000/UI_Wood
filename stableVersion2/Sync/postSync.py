7e597d29be8f0a72e61c714071fc8ddf21e008ef
ali <ali.s.tag2000@gmail.com>
2023-09-21 16:56:20 +0430

    Edit based on new post analysis.

--- stableVersion2/Sync/postSync.py
+++ stableVersion2/Sync/postSync.py
@@ -2,9 +2,8 @@ import sys
 
 sys.path.append(r"D:\git\Wood\UI_Wood\stableVersion2")
 from UI_Wood.stableVersion2.output.post_output import post_output
+from UI_Wood.stableVersion2.Sync.beamSync import roundAll
 from WOOD_DESIGN.mainpost import MainPost
-from Report_Lab.version1.main import Main
-from Report_Lab.version1.post.input import PostInput
 
 
 class PostSync:
@@ -13,16 +12,15 @@ class PostSync:
         print(self.postOutPut.postProperties)
         postId = 1
         for post in self.postOutPut.postProperties:
-            InputReport = PostInput(post, generalInfo)
-            value = InputReport.input
-            report = Main(value, value, value,
-                          f"D:/git/Wood/UI_Wood/stableVersion1/report/Post_output_{post['label']}_ID_{str(postId)}.pdf")
             postAnalysis = MainPost(post)
-            postAnalysis.query.insert(0, postId)
-            postAnalysis.query.insert(1, str(post["story"]))
-            postAnalysis.query.insert(2, post["label"])
-            db.cursor1.execute(
-                'INSERT INTO POST (ID, STORY,LABEL, SIZE, GRADE, Pa_k, fc_psi, Cp, P_allow, Fcc_psi, DCR, Load_comb, Fc_perp, Load_comb_sill, DCR_sill) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
-                postAnalysis.query)
-            db.conn1.commit()
-            postId += 1
+            if postAnalysis.query:
+                postAnalysis.query.insert(0, postId)
+                postAnalysis.query.insert(1, str(post["story"]))
+                postAnalysis.query.insert(2, post["label"])
+                newQuery = roundAll(postAnalysis.query)
+                db.cursor1.execute(
+                    'INSERT INTO POST (ID, STORY, LABEL, HEIGHT, WIDTH, SPECIES, SIZE, Pa_max, fc_psi, Cp, Pa_allow, Fc_allow, axial_dcr, axial_comb, fcperp_demand, fcperp_comb, fcperp_dcr, b, d, Fb, Ft, Fc, Fv, Fc_perp0, E, Emin, A, Sx, Sy, Ix, Iy, rx, ry, kx, ky, reaction1, reaction2, reaction3, reaction4, reaction5, reaction6, reaction7, max_axial_stress1, max_axial_stress2, max_axial_stress3, max_axial_stress4, '
+                    'max_axial_stress5, max_axial_stress6, max_axial_stress7, Cd, Ct, Cfb, Cfc, Cft, Ci, Cm, FCE, Fcstar, Fcperp_capacity, bend_capacity, shear_capacity) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
+                    newQuery)
+                db.conn1.commit()
+                postId += 1
