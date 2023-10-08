a2f6b970a6cf7125a0a77ea6855cdc57157b8bfd
ali <ali.s.tag2000@gmail.com>
2023-10-08 15:36:12 +0330

    Edit Sync classes. Now if element have been designed once, we will see just result and design procedure won't be run again.

--- stableVersion3/Sync/beamSync.py
+++ stableVersion3/Sync/beamSync.py
@@ -14,85 +14,98 @@ from PySide6.QtWidgets import QDialog
 
 
 class beamAnalysisSync:
-    def __init__(self, beam, Posts, ShearWalls, generalInfo, db, GridClass=None, storyBy=False, report=False):
+    def __init__(self, beam, Posts, ShearWalls, generalInfo, db, GridClass=None, storyBy=False, report=False,
+                 BeamStories=[]):
         self.beam = beam
         self.reactionTab = []
         self.reaction_list = []
         beamId = 1
         beamIdInput = 1
+        self.BeamStories = BeamStories
         self.report = report
-        inputDB = beamSQL()
-        for i, beamTab in enumerate(beam):
-            BeamStory = []
-            self.reaction_list.clear()
-            tabNumber = i
-            beam_support = []
+        if report:
+            for i, beamTab in enumerate(beam):
+                if storyBy:
+                    storyByStoryInstance = BeamStoryBy(self.BeamStories[i], GridClass)
+                    if i == len(beam) - 1:
+                        self.report = True
+                    if storyByStoryInstance.result == QDialog.Accepted:
+                        continue
+                    else:
+                        break
+        else:
+            inputDB = beamSQL()
+            for i, beamTab in enumerate(beam):
+                BeamStory = []
+                self.reaction_list.clear()
+                tabNumber = i
+                beam_support = []
 
-            for Beam in beamTab:
-                beamSupport = Beam["support"]
-                for support in beamSupport:
-                    supportLabel = support["label"]
-                    if "B" in supportLabel:
-                        beam_support.append(supportLabel)
+                for Beam in beamTab:
+                    beamSupport = Beam["support"]
+                    for support in beamSupport:
+                        supportLabel = support["label"]
+                        if "B" in supportLabel:
+                            beam_support.append(supportLabel)
 
-            beamDesignedList = []
+                beamDesignedList = []
 
-            # if beam support was an empty list
-            if not beam_support:
-                beam_support = "True"
-            while beam_support:
-                if beam_support == "True":
-                    beam_support = []
-                beam_support, self.reaction_list, beamDesignedList, reactionInstance, beamId = self.DesignPrimaryBeam(
-                    beam_support, beamId,
-                    self.reaction_list,
-                    tabNumber, db, beamTab,
-                    Posts, ShearWalls,
-                    beamDesignedList, BeamStory)
+                # if beam support was an empty list
+                if not beam_support:
+                    beam_support = "True"
+                while beam_support:
+                    if beam_support == "True":
+                        beam_support = []
+                    beam_support, self.reaction_list, beamDesignedList, reactionInstance, beamId = self.DesignPrimaryBeam(
+                        beam_support, beamId,
+                        self.reaction_list,
+                        tabNumber, db, beamTab,
+                        Posts, ShearWalls,
+                        beamDesignedList, BeamStory)
 
-            beamOutput = beam_output(beamTab)
-            for beamNum, beam_ in enumerate(beamOutput.beamProperties):
-                # beam with no support are empty(False)
-                if beam_:
-                    WriteBeamInputSQL(beam_, str(tabNumber + 1), beamIdInput, inputDB)
-                    beamIdInput += 1
-                    if beam_["label"] not in beamDesignedList:
-                        beam_analysis = MainBeam(beam_)
-                        if beam_analysis.query[0] != "No Section Was Adequate":
-                            figs = beam_analysis.plots
-                            figs[0].write_image(
-                                f"images/beam/Beam_external_story{tabNumber + 1}_label_{beam_['label']}.png")
-                            figs[1].write_image(
-                                f"images/beam/Beam_internal_story{tabNumber + 1}_label_{beam_['label']}.png")
-                            # WriteBeamInputSQL(beam_, str(tabNumber + 1), beamId, inputDB)
+                beamOutput = beam_output(beamTab)
+                for beamNum, beam_ in enumerate(beamOutput.beamProperties):
+                    # beam with no support are empty(False)
+                    if beam_:
+                        WriteBeamInputSQL(beam_, str(tabNumber + 1), beamIdInput, inputDB)
+                        beamIdInput += 1
+                        if beam_["label"] not in beamDesignedList:
+                            beam_analysis = MainBeam(beam_)
+                            if beam_analysis.query[0] != "No Section Was Adequate":
+                                figs = beam_analysis.plots
+                                figs[0].write_image(
+                                    f"images/beam/Beam_external_story{tabNumber + 1}_label_{beam_['label']}.png")
+                                figs[1].write_image(
+                                    f"images/beam/Beam_internal_story{tabNumber + 1}_label_{beam_['label']}.png")
+                                # WriteBeamInputSQL(beam_, str(tabNumber + 1), beamId, inputDB)
 
-                            beam_analysis.query.insert(0, str(beamId))
-                            beam_analysis.query.insert(1, str(tabNumber + 1))
-                            beam_analysis.query.insert(2, beam_["label"])
-                            newQuery = roundAll(beam_analysis.query)
+                                beam_analysis.query.insert(0, str(beamId))
+                                beam_analysis.query.insert(1, str(tabNumber + 1))
+                                beam_analysis.query.insert(2, beam_["label"])
+                                newQuery = roundAll(beam_analysis.query)
 
-                            db.cursor1.execute(
-                                'INSERT INTO BEAM (ID, STORY, LABEL, SPECIES, SPANS, LENGTH, LOAD_COMB, SIZE, Vmax, Mmax, Fb_actual, Fb_allow, Fv_actual, Fv_allow, Deflection_actual, Deflection_allow, Bending_dcr, Shear_dcr, defl_dcr, DIST_D, DIST_D_range, DIST_L, DIST_L_range, DIST_LR, DIST_LR_range, DIST_E, DIST_E_range, DIST_S, DIST_S_range, P_D, P_D_range, P_L, P_L_range, P_LR, P_LR_range, P_E, P_E_range, P_S, P_S_range, RD, RL, RLr, RE, RS, Mmax_loc, Vmax_loc, d, b, Fb, Ft, Fc, Fv, Fcperp, E, Emin, A, Sx, Sy, Ix, Iy, Cd, Ct, Cfb, Cfc, Cft, Cfu, Ci, Ciperp, Cr, Cb, Cl, Fcperp_cap, Fcperp_dem) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
-                                newQuery)
-                            db.conn1.commit()
-                            beam_["bending_dcr"] = newQuery[16]
-                            beam_["shear_dcr"] = newQuery[17]
-                            beam_["deflection_dcr"] = newQuery[18]
-                            beam_["size"] = newQuery[7]
-                            BeamStory.append(beam_)
-                        Control_reaction(beam_analysis.output.post_output, beamTab[beamNum], self.reaction_list)
-                        beamId += 1
-
-            reactionInstance.do_post()
-            self.reactionTab.append(self.reaction_list)
-            if storyBy:
-                storyByStoryInstance = BeamStoryBy(BeamStory, GridClass)
-                if i == len(beam) - 1:
-                    self.report = True
-                if storyByStoryInstance.result == QDialog.Accepted:
-                    continue
-                else:
-                    break
+                                db.cursor1.execute(
+                                    'INSERT INTO BEAM (ID, STORY, LABEL, SPECIES, SPANS, LENGTH, LOAD_COMB, SIZE, Vmax, Mmax, Fb_actual, Fb_allow, Fv_actual, Fv_allow, Deflection_actual, Deflection_allow, Bending_dcr, Shear_dcr, defl_dcr, DIST_D, DIST_D_range, DIST_L, DIST_L_range, DIST_LR, DIST_LR_range, DIST_E, DIST_E_range, DIST_S, DIST_S_range, P_D, P_D_range, P_L, P_L_range, P_LR, P_LR_range, P_E, P_E_range, P_S, P_S_range, RD, RL, RLr, RE, RS, Mmax_loc, Vmax_loc, d, b, Fb, Ft, Fc, Fv, Fcperp, E, Emin, A, Sx, Sy, Ix, Iy, Cd, Ct, Cfb, Cfc, Cft, Cfu, Ci, Ciperp, Cr, Cb, Cl, Fcperp_cap, Fcperp_dem) values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
+                                    newQuery)
+                                db.conn1.commit()
+                                beam_["bending_dcr"] = newQuery[16]
+                                beam_["shear_dcr"] = newQuery[17]
+                                beam_["deflection_dcr"] = newQuery[18]
+                                beam_["size"] = newQuery[7]
+                                BeamStory.append(beam_)
+                            Control_reaction(beam_analysis.output.post_output, beamTab[beamNum], self.reaction_list)
+                            beamId += 1
+                self.BeamStories.append(BeamStory)
+                reactionInstance.do_post()
+                self.reactionTab.append(self.reaction_list)
+                if storyBy:
+                    storyByStoryInstance = BeamStoryBy(BeamStory, GridClass)
+                    if i == len(beam) - 1:
+                        self.report = True
+                    if storyByStoryInstance.result == QDialog.Accepted:
+                        continue
+                    else:
+                        break
 
     @staticmethod
     def DesignPrimaryBeam(beam_support, beamId, reaction_list, tabNumber, db, beamTab, Posts, ShearWalls, beamDesigned,
--- stableVersion3/Sync/postSync2.py
+++ stableVersion3/Sync/postSync2.py
@@ -12,44 +12,58 @@ from PySide6.QtWidgets import QDialog
 class PostSync2:
     def __init__(self, GridClass, beam, Posts, ShearWalls, height, general_information, db,
                  reportPost=False,
-                 reportBeam=False):
+                 reportBeam=False, PostStories=[], BeamStories=[]):
         self.GridClass = GridClass
-        postId = 1
-        for story, post in enumerate(Posts):
-            postItem = list(post.values())[0]
-            # for story, postItem in post.items():
-            post_forBeamDesign = [{story: postItem}]
-            beam_forBeamDesign = [beam[story]]
-            shearWall_forBeamDesign = [ShearWalls[story]]
-            if not reportBeam:
-                beamAnalysisInstance = beamAnalysisSync(beam_forBeamDesign, post_forBeamDesign, shearWall_forBeamDesign,
-                                                        general_information,
-                                                        db)
-            self.postOutPut = post_output(post_forBeamDesign, height)
-            self.reportPost = reportPost
-            self.reportBeam = reportBeam
-            postStory = []
-            for postEdited in self.postOutPut.postProperties:
-                postAnalysis = MainPost(postEdited)
-                if postAnalysis.query:
-                    postAnalysis.query.insert(0, postId)
-                    postAnalysis.query.insert(1, str(postEdited["story"]))
-                    postAnalysis.query.insert(2, postEdited["label"])
-                    newQuery = roundAll(postAnalysis.query)
-                    db.cursor1.execute(
-                        'INSERT INTO POST (ID, STORY, LABEL, HEIGHT, WIDTH, SPECIES, SIZE, Pa_max, fc_psi, Cp, Pa_allow, Fc_allow, axial_dcr, axial_comb, fcperp_demand, fcperp_comb, fcperp_dcr, b, d, Fb, Ft, Fc, Fv, Fc_perp0, E, Emin, A, Sx, Sy, Ix, Iy, rx, ry, kx, ky, reaction1, reaction2, reaction3, reaction4, reaction5, reaction6, reaction7, max_axial_stress1, max_axial_stress2, max_axial_stress3, max_axial_stress4, '
-                        'max_axial_stress5, max_axial_stress6, max_axial_stress7, Cd, Ct, Cfb, Cfc, Cft, Ci, Cm, FCE, Fcstar, Fcperp_capacity, bend_capacity, shear_capacity) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
-                        newQuery)
-                    db.conn1.commit()
-                    postId += 1
-                    postEdited["axial_dcr"] = newQuery[12]
-                    postStory.append(postEdited)
-
-            postStoryDesigned = PostStoryBy(postStory, GridClass)
-            if story == len(beam) - 1:
-                self.reportPost = True
-                self.reportBeam = True
-            if postStoryDesigned.result == QDialog.Accepted:
-                continue
-            else:
-                break
+        self.PostStories = PostStories
+        self.BeamStories = BeamStories
+        if reportPost:
+            for story, post in enumerate(Posts):
+                postStoryDesigned = PostStoryBy(self.PostStories[story], GridClass)
+                if story == len(beam) - 1:
+                    self.reportPost = True
+                    self.reportBeam = True
+                if postStoryDesigned.result == QDialog.Accepted:
+                    continue
+                else:
+                    break
+        else:
+            postId = 1
+            for story, post in enumerate(Posts):
+                postItem = list(post.values())[0]
+                # for story, postItem in post.items():
+                post_forBeamDesign = [{story: postItem}]
+                beam_forBeamDesign = [beam[story]]
+                shearWall_forBeamDesign = [ShearWalls[story]]
+                if not reportBeam:
+                    beamAnalysisInstance = beamAnalysisSync(beam_forBeamDesign, post_forBeamDesign, shearWall_forBeamDesign,
+                                                            general_information,
+                                                            db)
+                    self.BeamStories.append(beamAnalysisInstance.BeamStories[0])
+                self.postOutPut = post_output(post_forBeamDesign, height)
+                self.reportPost = reportPost
+                self.reportBeam = reportBeam
+                postStory = []
+                for postEdited in self.postOutPut.postProperties:
+                    postAnalysis = MainPost(postEdited)
+                    if postAnalysis.query:
+                        postAnalysis.query.insert(0, postId)
+                        postAnalysis.query.insert(1, str(postEdited["story"]))
+                        postAnalysis.query.insert(2, postEdited["label"])
+                        newQuery = roundAll(postAnalysis.query)
+                        db.cursor1.execute(
+                            'INSERT INTO POST (ID, STORY, LABEL, HEIGHT, WIDTH, SPECIES, SIZE, Pa_max, fc_psi, Cp, Pa_allow, Fc_allow, axial_dcr, axial_comb, fcperp_demand, fcperp_comb, fcperp_dcr, b, d, Fb, Ft, Fc, Fv, Fc_perp0, E, Emin, A, Sx, Sy, Ix, Iy, rx, ry, kx, ky, reaction1, reaction2, reaction3, reaction4, reaction5, reaction6, reaction7, max_axial_stress1, max_axial_stress2, max_axial_stress3, max_axial_stress4, '
+                            'max_axial_stress5, max_axial_stress6, max_axial_stress7, Cd, Ct, Cfb, Cfc, Cft, Ci, Cm, FCE, Fcstar, Fcperp_capacity, bend_capacity, shear_capacity) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
+                            newQuery)
+                        db.conn1.commit()
+                        postId += 1
+                        postEdited["axial_dcr"] = newQuery[12]
+                        postStory.append(postEdited)
+                self.PostStories.append(postStory)
+                postStoryDesigned = PostStoryBy(postStory, GridClass)
+                if story == len(beam) - 1:
+                    self.reportPost = True
+                    self.reportBeam = True
+                if postStoryDesigned.result == QDialog.Accepted:
+                    continue
+                else:
+                    break
