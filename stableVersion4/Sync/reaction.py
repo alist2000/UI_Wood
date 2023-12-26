143e424ad7d09f454ec25843aafbec7ed9839d05
ali <ali.s.tag2000@gmail.com>
2023-12-26 15:34:45 +0330

    control reaction for inclined beams. Assign reaction loads as a point load on supports based on distance from start for inclined beams.

--- stableVersion4/Sync/reaction.py
+++ stableVersion4/Sync/reaction.py
@@ -1,7 +1,9 @@
 import sys
+import numpy as np
 
 sys.path.append(r"D:\git\Wood\UI_Wood\11.5")
-from post_new import magnification_factor
+from UI_Wood.stableVersion4.post_new import magnification_factor
+from UI_Wood.stableVersion4.back.load_control import distance
 
 
 class reaction_types:
@@ -24,11 +26,27 @@ class Control_reaction(reaction_types):
         if direction == "N-S":
             self.direction_index = 1
             self.constant_index = 0
-        else:
+            self.start_coordinate = min(coordinate_beam[0][self.direction_index],
+                                        coordinate_beam[1][self.direction_index])
+
+        elif direction == "E-W":
             self.direction_index = 0
             self.constant_index = 1
+            self.start_coordinate = min(coordinate_beam[0][self.direction_index],
+                                        coordinate_beam[1][self.direction_index])
 
-        self.start_coordinate = min(coordinate_beam[0][self.direction_index], coordinate_beam[1][self.direction_index])
+        else:  # inclined
+            self.direction_index = 2
+            self.constant_index = 2
+            one = coordinate_beam[0]
+            two = coordinate_beam[1]
+            if one[0] <= two[0]:
+                self.start_coordinate_main = one
+                self.start_coordinate = one[0]
+            else:
+                self.start_coordinate_main = two
+                self.start_coordinate = two[0]
+
         # for i, load in enumerate(self.loads):
         for loc, load in reactions.items():
             self.control_every_reaction(loc, load)
@@ -40,20 +58,35 @@ class Control_reaction(reaction_types):
 
     def control_every_reaction(self, loc, load):
         reaction_distance = (float(loc) * magnification_factor) + self.start_coordinate
-        if self.direction_index == 1:
-            reaction_coordinate = (round(self.coordinate_beam[0][self.constant_index], 0), round(reaction_distance, 0))
-        else:
-            reaction_coordinate = (round(reaction_distance, 0), round(self.coordinate_beam[0][self.constant_index], 0))
         support = self.selected_beam["support"]
 
-        for i, magnitude in enumerate(load):
-            load_type = self.load_names[i]
-            for supportItem in support:
-                if supportItem.get("reaction") is None:
-                    supportItem["reaction"] = []
-                coordinate = tuple([round(i, 0) for i in supportItem["coordinate"]])
-                if coordinate == reaction_coordinate:
-                    supportItem["reaction"].append({"magnitude": magnitude, "type": load_type})
+        if self.direction_index == 1 or self.direction_index == 0:
+            if self.direction_index == 1:
+                reaction_coordinate = (
+                    round(self.coordinate_beam[0][self.constant_index], 0), round(reaction_distance, 0))
+            else:
+                reaction_coordinate = (
+                    round(reaction_distance, 0), round(self.coordinate_beam[0][self.constant_index], 0))
+
+            for i, magnitude in enumerate(load):
+                load_type = self.load_names[i]
+                for supportItem in support:
+                    if supportItem.get("reaction") is None:
+                        supportItem["reaction"] = []
+                    coordinate = tuple([round(i, 0) for i in supportItem["coordinate"]])
+                    if coordinate == reaction_coordinate:
+                        supportItem["reaction"].append({"magnitude": magnitude, "type": load_type})
+        else:  # Inclined beam
+            for i, magnitude in enumerate(load):
+                load_type = self.load_names[i]
+                for supportItem in support:
+                    if supportItem.get("reaction") is None:
+                        supportItem["reaction"] = []
+                    coordinate = tuple([round(i, 0) for i in supportItem["coordinate"]])
+                    SupportDist = distance(self.start_coordinate_main, coordinate) + self.start_coordinate
+                    error = magnification_factor / 10
+                    if abs(reaction_distance - SupportDist) <= error:
+                        supportItem["reaction"].append({"magnitude": magnitude, "type": load_type})
 
 
 class Reaction_On:
