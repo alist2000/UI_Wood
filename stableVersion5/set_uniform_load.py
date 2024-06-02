5c21a6c0555f1b779520a1d1a6b55e2bab55cd2f
ali <ali.s.tag2000@gmail.com>
2024-06-02 18:22:54 +0330

    Add select weather reducible or not option.

--- stableVersion5/set_uniform_load.py
+++ stableVersion5/set_uniform_load.py
@@ -17,8 +17,9 @@ class set_uniform_load(QDialog):
         self.EditLoadMapLoadSet = None
         self.loadSetId = uuid.uuid4()
         self.set_uniforms = None
-        self.load_data = {"id": self.loadSetId, "name": "", "properties": []}
+        self.load_data = {"id": self.loadSetId, "name": "", "properties": [], "Reducible": True}
         self.all_set_load = {}
+        self.all_set_load2 = {}
         self.mainPage = mainPage
 
         self.setWindowTitle("Set Uniform Load")
@@ -56,6 +57,7 @@ class set_uniform_load(QDialog):
                     self.load_data["name"] = name + " "
 
             self.all_set_load[str(self.loadSetId)] = {self.load_data["name"]: self.load_data["properties"]}
+            self.all_set_load2[str(self.loadSetId)] = {self.load_data["name"]: self.load_data["Reducible"]}
             for load in self.load_data["properties"]:
                 loadType = load["type"]
                 if loadType == "Dead Super":
@@ -71,6 +73,7 @@ class set_uniform_load(QDialog):
                     "but super dead loads are left empty, They will be assumed 10 psf greater than dead loads.</li></ul></body></html>")
             self.listWidget.addItem(item)
             self.EditLoadMapLoadSet.edit(self.all_set_load)
+            # self.EditLoadMapLoadSet.edit(self.all_set_load2)
 
     def delete_row(self):
         # Delete the selected row
@@ -80,6 +83,7 @@ class set_uniform_load(QDialog):
                 nameItem = list(items.keys())[0]
                 if nameItem == name:
                     self.all_set_load.pop(keys)
+                    self.all_set_load2.pop(keys)
                     self.listWidget.takeItem(self.listWidget.row(item))
                     break
 
@@ -94,7 +98,8 @@ class set_uniform_load(QDialog):
                 nameItem = list(items.keys())[0]
                 if nameItem == name:
                     load_data = list(self.all_set_load[keys].values())[0]
-                    load_data_dict = {"name": name, "properties": load_data}
+                    reducible = list(self.all_set_load2[keys].values())[0]
+                    load_data_dict = {"name": name, "properties": load_data, "Reducible": reducible}
                     dialog = UniformLoadDialog(load_data_dict)
                     dialog.button_box.accepted.connect(
                         dialog.accept_control)  # Change from dialog.accept to self.accept
@@ -103,6 +108,7 @@ class set_uniform_load(QDialog):
 
             if dialog.exec() == QDialog.Accepted:
                 self.all_set_load.pop(mainId)
+                self.all_set_load2.pop(mainId)
 
                 name = dialog.uniform_load.load_data["name"]
                 for i in self.all_set_load.keys():
@@ -111,6 +117,8 @@ class set_uniform_load(QDialog):
 
                 self.all_set_load[mainId] = {
                     dialog.uniform_load.load_data["name"]: dialog.uniform_load.load_data["properties"]}
+                self.all_set_load2[mainId] = {
+                    dialog.uniform_load.load_data["name"]: dialog.uniform_load.load_data["Reducible"]}
 
                 # Delete
                 self.listWidget.takeItem(self.listWidget.row(item))
@@ -131,6 +139,7 @@ class set_uniform_load(QDialog):
                 self.listWidget.addItem(item)
                 self.load_data = dialog.uniform_load.load_data
                 self.EditLoadMapLoadSet.edit(self.all_set_load)
+                # self.EditLoadMapLoadSet.edit(self.all_set_load2)
 
     # SLOT
     def uniform_load_exe(self):
@@ -175,6 +184,11 @@ class UniformLoad(QWidget):
         self.tableWidget.insertRow(0)
         self.tableWidget.setCellWidget(0, 0, QLabel("Name"))
         self.tableWidget.setCellWidget(0, 1, QTextEdit())
+        self.tableWidget.insertRow(1)
+        self.tableWidget.setCellWidget(1, 0, QLabel("Reducible Live?"))
+        self.combo = QComboBox()
+        self.combo.addItems(["Yes", "No"])
+        self.tableWidget.setCellWidget(1, 1, self.combo)
 
         self.tableWidget.setSelectionBehavior(QAbstractItemView.SelectRows)
         self.tableWidget.horizontalHeader().setVisible(False)  # hide column headers
@@ -194,9 +208,10 @@ class UniformLoad(QWidget):
         print(self.load_data)
         for i, row_data in enumerate(self.load_data["properties"]):
             loadName = self.load_data["name"]
-            self.add_item(loadName, row_data, i)
+            reducible = self.load_data["Reducible"]
+            self.add_item(loadName, reducible, row_data, i)
 
-    def add_item(self, name=None, row_data=None, i=0):
+    def add_item(self, name=None, reducible=None, row_data=None, i=0):
         print("row data", row_data)
         row = self.tableWidget.rowCount()
         self.tableWidget.insertRow(row)
@@ -226,6 +241,19 @@ class UniformLoad(QWidget):
             if i == 0:
                 self.tableWidget.setCellWidget(0, 0, QLabel("Name"))
                 self.tableWidget.setCellWidget(0, 1, QTextEdit(name))
+                # self.tableWidget.setCellWidget(1, 0, QLabel("Reducible Live?"))
+                # combo = QComboBox()
+                if reducible is True:
+                    name1 = "Yes"
+                    name2 = "No"
+                else:
+                    name1 = "No"
+                    name2 = "Yes"
+                self.combo.setCurrentText(name1)
+                # combo.addItems([name2, name1])
+                # combo.setCurrentText(name1)
+                # self.tableWidget.setCellWidget(1
+                #                                , 1, combo)
             comboBox.setCurrentText(row_data['type'])
             spinBox.setValue(row_data['magnitude'])
 
@@ -241,8 +269,14 @@ class UniformLoad(QWidget):
         self.load_data.clear()  # clear the old values
 
         self.load_data["name"] = self.tableWidget.cellWidget(0, 1).toPlainText()
+        reducible = self.tableWidget.cellWidget(1, 1).currentText()
+        if reducible == "Yes":
+            reducible = True
+        else:
+            reducible = False
+        self.load_data["Reducible"] = reducible
         self.load_data["properties"] = []
-        for row in range(1, self.tableWidget.rowCount()):
+        for row in range(2, self.tableWidget.rowCount()):
             comboBox = self.tableWidget.cellWidget(row, 0)
             spinBox = self.tableWidget.cellWidget(row, 1)
             loadType = comboBox.currentText()
