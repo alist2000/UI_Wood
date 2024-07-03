79159510cf7130747254285685ce356ab834326a
ali <ali.s.tag2000@gmail.com>
2024-07-03 15:46:34 +0330

    Debug.

--- stableVersion5/main.py
+++ stableVersion5/main.py
@@ -1,11 +1,12 @@
-from PySide6.QtWidgets import QApplication
-from welcome import Welcome
-from tab_widget import tabWidget
-import sys
 from path import main_path, directories, HandleDirectories
 
 if __name__ == "__main__":
     main_path()
+    from PySide6.QtWidgets import QApplication
+    from welcome import Welcome
+    from tab_widget import tabWidget
+    import sys
+
     dirs = HandleDirectories(directories)
     dirs.make_all()
     app = QApplication(sys.argv)
