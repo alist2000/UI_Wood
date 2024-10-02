49c5e12465c0e4fe4cee31f2388a193b17005d1d
Ali <ali.s.tag2000@gmail.com>
2024-10-02 15:13:58 +0330

    Start working on VERSION 24.2.10. for using last stable version checkout version before this commit. copy_and_rename_databases defined to copy database.
    create_seismic_parameters defined to create seismic parameters dictionary from database.

---
+++ stableVersion5/run/shearWall_db_base.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+import sqlite3
+
+
+# Function to copy and rename SQLite databases
+def copy_and_rename_databases(db1, db2, new_name1, new_name2):
+    if not os.path.exists(new_name1):
+        os.makedirs(new_name1)
+
+    new_path1 = new_name1
+    # Copy the first SQLite database
+    shutil.copy(db1, new_path1)
+    print(f"Copied {db1} to {new_path1}")
+
+
+def create_seismic_parameters(database):
+    # Connect to SQLite database
+    conn = sqlite3.connect(database)
+    # Create a cursor object
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM 'seismicParams'")
+    S1, Ss, Fa, Fv, I, Tmodel, R, risk, regular = cursor.fetchone()
+
+    cursor.close()
+    return {
+        'Fa': Fa, 'Fv': Fv, 'I': I, 'R Factor': R, 'Regular Building': regular, 'Risk Category': risk,
+        'S1': S1, 'Ss': Ss, 'T model': Tmodel
+    }
